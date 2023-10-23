68cf6baf03cf1af1e90c086d6007e3a5f6754752
simon <22402942+s7m0n@users.noreply.github.com>
2023-10-23 16:55:27 +0200

    update only if ip changed and add help, options for dry-run and force update

--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,144 @@
-use reqwest::Client;
-// use tokio::main;
-use std::error::Error;
+use chrono::{DateTime, Utc};
+use clap::{App, Arg};
 use config::{Config, File};
-// use ipnetwork::IpNetwork;
-use serde::Deserialize;  // Import Deserialize from serde
 use dirs;
 use get_if_addrs::get_if_addrs;
-use std::net::{IpAddr, Ipv6Addr};
+use reqwest::Client;
+use serde::Deserialize;
+use std::error::Error;
 use std::fs;
-//use chrono::prelude::*;
-use chrono::{DateTime, FixedOffset, Utc};
-
+use std::net::{IpAddr, Ipv6Addr};
+use std::path::Path;
 
 #[tokio::main]
-async fn main()->Result<(), Box<dyn Error>> {
-    // Get the user's home directory
-    if let
-        Some(home_dir) = dirs::home_dir() {
-            let config_file_path =
-                home_dir.join(".config/dnsupdaterconfig.toml");
-            let mut settings = Config::default();
-            settings.merge(File::from(config_file_path))         ? ;
-            let config = settings.try_into::<YourConfigStruct>() ? ;
+async fn main() -> Result<(), Box<dyn Error>> {
+    let matches = App::new("DNS Updater")
+        .version("1.0")
+        .author("")
+        .about("Updates DNS records with the current IP address")
+        .arg(
+            Arg::with_name("dry-run")
+                .short("n")
+                .long("dry-run")
+                .help("Run in dry-run mode (no actual update)"),
+        )
+        .arg(
+            Arg::with_name("force")
+                .short("f")
+                .long("force")
+                .help("Force the update even if the IP address hasn't changed"),
+        )
+        .get_matches();
 
-            // Get the IPv6 address of the specified interface
-            let mut ip6addr = get_interface_ipv6_address(&config.interface) ? ;
-          
+    let is_dry_run = matches.is_present("dry-run");
+    let is_force = matches.is_present("force");
 
-            // Read the previous IP address and timestamp from the status file
-            let(prev_ip, prev_time) = read_status_file(home_dir.as_path())?;
+    let system_config_path = Path::new("/etc/dnsupdaterconfig.toml");
 
-            // Check if the current IP address is the same as the previous one
-            if ip6addr.to_string() != prev_ip {
-                    // Build the URL with dynamic parameters
-                    let url = format !(
-                        "https://{}:{}@update.spdyn.de/nic/update?hostname={}&myip={}",
-                        config.username, config.password, config.domain,
-                        ip6addr.to_string());
-                    let urlnopass = format !(
-                        "https://{}:{}@update.spdyn.de/nic/update?hostname={}&myip={}",
-                        config.username, "-hidden-", config.domain,
-                        ip6addr.to_string());
+    let user_config = load_user_config()?;
+    let system_config = load_system_config(system_config_path)?;
+    let mut settings = user_config.clone();
+    settings.merge(system_config)?;
+    let config = settings.try_into::<YourConfigStruct>()?;
 
-                    // Create an HTTP client
-                    let client = Client::new ();
+    // Read the previous IP address and timestamp from the status file
+    let status_file_path = config
+        .statusFilePath
+        .as_ref()
+        .map_or("/var/cache/dnsupdater/status", |path| path.as_str());
 
-                    println !("would Update using: {}", &urlnopass);
-                    // Return early and don't execute the code below
-                    return Ok(());
-                    // Make the HTTPS request
-                    let response = client.get(&url).send().await ? ;
+    // Get the IPv6 address of the specified interface
+    let ip6addr = get_interface_ipv6_address(&config.interface)?;
 
-                    // Check and handle the response
-                    if response.status().is_success() {
-                            write_status_file(home_dir.as_path(), ip6addr.to_string()) ? ;
-                            println !("Update successful! using:{} ",
-                                      urlnopass);
-                        }
-                    else {
-                        println !("Update failed. Status: {}",
-                                  response.status());
-                    }
-                }
-            else {
-                println !("IP address has not changed. Skipping update.");
-            } 
-            Ok(())
-        } 
-    else {
-        eprintln !("Failed to determine the user's home directory.");
-        Ok(())
+    // Read the previous IP address and timestamp from the status file
+    let (prev_ip, prev_time) = read_status_file(status_file_path)?;
+
+    // Check if the current IP address is the same as the previous one
+    if ip6addr.to_string() == prev_ip && !is_force {
+        println!(
+            "IP address has not changed since last update:{}. Skipping update.",
+            prev_time
+        );
+    } else {
+        // Build the URL with dynamic parameters
+        let url = format!(
+            "https://{}:{}@update.spdyn.de/nic/update?hostname={}&myip={}",
+            config.username,
+            config.password,
+            config.domain,
+            ip6addr.to_string()
+        );
+        let urlnopass = format!(
+            "https://{}:{}@update.spdyn.de/nic/update?hostname={}&myip={}",
+            config.username,
+            "-hidden-",
+            config.domain,
+            ip6addr.to_string()
+        );
+
+        // Create an HTTP client
+        let client = Client::new();
+
+        println!("would Update using: {}", &urlnopass);
+        // Return early and don't execute the code below
+
+        // Make the HTTPS request
+        if is_dry_run {
+            write_status_file(status_file_path, ip6addr.to_string())?;
+            println!("Dry run done with updating status file!");
+        } else {
+            let response = client.get(&url).send().await?;
+            if response.status().is_success() {
+                write_status_file(status_file_path, ip6addr.to_string())?;
+                println!("Update successful! using:{} ", urlnopass);
+            } else {
+                println!("Update failed. Status: {}", response.status());
+            }
+        }
     }
+    Ok(())
 }
 
-fn get_interface_ipv6_address(interface_name
-                              : &str)
-    ->Result<Ipv6Addr, Box<dyn Error>> {
+fn get_interface_ipv6_address(interface_name: &str) -> Result<Ipv6Addr, Box<dyn Error>> {
     // Retrieve the list of network interfaces and their addresses
-    let interfaces = get_if_addrs() ? ;
+    let interfaces = get_if_addrs()?;
 
     // Iterate through all interfaces with a matching name
-    for
-        interface in interfaces.iter().filter(| ifaddr |
-                                              ifaddr.name == interface_name) {
-            // Check if the interface has any non-local IPv6 addresses
-            if let
-                ipv6_addr = interface.ip() {
-                    if ipv6_addr
-                        .is_ipv6() && !ipv6_addr.is_loopback() {
-                            if let
-                                IpAddr::V6(ipv6) = ipv6_addr {
-                                    return Ok(ipv6);
-                                }
-                        }
-                }
+    for interface in interfaces
+        .iter()
+        .filter(|ifaddr| ifaddr.name == interface_name)
+    {
+        // Check if the interface has any non-local IPv6 addresses
+        let ipv6_addr = interface.ip();
+        if ipv6_addr.is_ipv6() && !ipv6_addr.is_loopback() {
+            if let IpAddr::V6(ipv6) = ipv6_addr {
+                return Ok(ipv6);
+            }
         }
+    }
     Err("IPv6 address not found for the specified interface".into())
 }
 
+fn load_user_config() -> Result<Config, config::ConfigError> {
+    let mut config = Config::default();
+    if let Some(home_dir) = dirs::home_dir() {
+        let user_config_path = home_dir.join(".config/dnsupdaterconfig.toml");
+        if user_config_path.exists() {
+            config.merge(File::from(user_config_path))?;
+        }
+    }
+    Ok(config)
+}
+fn load_system_config(system_config_path: &Path) -> Result<Config, config::ConfigError> {
+    let mut config = Config::default();
+    if system_config_path.exists() {
+        config.merge(File::from(system_config_path))?;
+    }
+    Ok(config)
+}
 
-fn read_status_file(home_dir: &std::path::Path) -> Result<(String, DateTime<Utc>), Box<dyn Error>> {
-    let status_file_path = home_dir.join(".config/dnsupdater_status.txt");
-    if let Ok(file_content) = fs::read_to_string(&status_file_path) {
+fn read_status_file(status_file_path: &str) -> Result<(String, DateTime<Utc>), Box<dyn Error>> {
+    if let Ok(file_content) = fs::read_to_string(status_file_path) {
         if let Some((ip, timestamp_str)) = file_content.lines().next().map(|line| {
             let mut parts = line.split(',');
             let ip = parts.next().unwrap_or("");
@@ -115,23 +152,20 @@ fn read_status_file(home_dir: &std::path::Path) -> Result<(String, DateTime<Utc>
     Ok(("".to_string(), Utc::now()))
 }
 
-fn write_status_file(home_dir
-                     : &std::path::Path, ip
-                     : String)
-    ->Result<(), Box<dyn Error>> {
-    let status_file_path = home_dir.join(".config/dnsupdater_status.txt");
+fn write_status_file(status_file_path: &str, ip: String) -> Result<(), Box<dyn Error>> {
     let timestamp = Utc::now();
-    let status = format !("{},{}\n", ip, timestamp.to_rfc3339());
-    fs::write(status_file_path, status) ? ;
+    let status = format!("{},{}\n", ip, timestamp.to_rfc3339());
+    fs::write(status_file_path, status)?;
+
     Ok(())
 }
-#[derive(Debug, Deserialize)]//
+
+#[derive(Debug, Deserialize)] //
 struct YourConfigStruct {
     domain: String,
     interface: String,
     username: String,
     password: String,
-    //ipaddress: String,
+    statusFilePath: Option<String>,
     // Add more fields as needed for your configuration
 }
-
