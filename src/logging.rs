85c66fd4113a54e7b9d01ccf57022ea8ece6842c
simon <22402942+s7m0n@users.noreply.github.com.>
2025-08-01 10:19:00 +0200

    remove log entry for logfile

--- src/logging.rs
+++ src/logging.rs
@@ -35,7 +35,7 @@ impl FileLogger {
 
 impl Logger for FileLogger {
     fn log(&self, message: &str) {
-        println!("Logging to file: {}", self.file_path);
+        //println!("Logging to file: {}", self.file_path);
         let now = Local::now();
         let timestamp = now.format("%Y-%m-%d %H:%M:%S").to_string();
         let log_line = format!("{}: {}", timestamp, message);
